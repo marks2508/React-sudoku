10838fac51fcd7d97cfbc34c113064eb518a952f
Mark S <markscicluna@hotmail.com>
2018-12-03 11:40:37 +0000

    added components and actions

---
+++ src/actions/grid.js
@@ -0,0 +1,26 @@
+export function inputValue(row, col, val) {
+  return {
+    type: 'INPUT_VALUE',
+    row,
+    col,
+    val
+  };
+}
+
+export function solve() {
+  return {
+    type: 'SOLVE'
+  };
+}
+
+export function clear() {
+  return {
+    type: 'CLEAR'
+  };
+}
+
+export function undo() {
+  return {
+    type: 'UNDO'
+  };
+}
